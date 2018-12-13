a0796cbb247294d4ac59f5b443af5446373fbbe6
Bohdan Sushchak <bogdansushak@gmail.com>
2018-12-13 13:08:27 +0200

    Implemented adding image to user account

--- app/src/main/java/com/sushchak/bohdan/firemessage/ui/MainActivity.kt
+++ app/src/main/java/com/sushchak/bohdan/firemessage/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sushchak.bohdan.firemessage.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.sushchak.bohdan.firemessage.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,11 +25,20 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.navigation_my_account -> {
-
+                supportFragmentManager.fragments
+                replaceFragment(MyAccountFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
         }
         false
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_layout, fragment)
+            commit()
+        }
     }
 }
--- app/src/main/java/com/sushchak/bohdan/firemessage/ui/SplashActivity.kt
+++ app/src/main/java/com/sushchak/bohdan/firemessage/ui/SplashActivity.kt
@@ -3,8 +3,6 @@ package com.sushchak.bohdan.firemessage.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
-import com.sushchak.bohdan.firemessage.ui.MainActivity
-import com.sushchak.bohdan.firemessage.ui.SignInActivity
 import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
